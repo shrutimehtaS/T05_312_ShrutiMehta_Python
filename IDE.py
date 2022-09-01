8f5c220534213242d12a54d945391bd98079193c
shrutimehtaS <111172882+shrutimehtaS@users.noreply.github.com>
2022-09-02 01:01:59 +0530

    Add files via upload

---
+++ IDE.py
@@ -0,0 +1,19 @@
+""""
+1. Advantages of IDE - IDE has the ability to correct syntaxes, gives a warning about memory leaks,
+assist in writing quality of code, etc. It has increased efficiency, where you can code faster with less effort,
+and its features help organise resources, prevent mistakes, and provide shortcuts
+=================================================================================================================
+2. IDE Shortcuts in PyCharm  -
+double shift - search everywhere
+ctrl+shift+A - find action
+Alt+Enter    - Show Context Actions
+F2           - Navigate between code issues
+Shift+F2     - Jump to the next or previous highlighted error
+Ctrl+E       - View recent files
+Ctrl+W       - Extend or shrink selection
+Ctrl+Shift+W - Increase or decrease the scope of selection according to specific code constructs.
+Ctrl+/       - Add/remove line or block comment
+Ctrl+Shift+/ - Comment out a line or block of code.
+Alt+F7       - Find Usages
+====================================================================================================================
+ """
