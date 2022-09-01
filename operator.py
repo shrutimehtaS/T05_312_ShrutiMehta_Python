a814230d2834e2c4d12173ee7143c394084e8f4a
shrutimehtaS <111172882+shrutimehtaS@users.noreply.github.com>
2022-09-02 01:03:44 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,22 @@
+"""OPERATORS IN PYTHON :-An operator is a symbol that perform an operation.python have many operator
+1. Arithmatic operators - Arithmatic operators are used to perform basic arithmetic operations like +,-,*,/,//,%,**
+/ // % DIFFERENCES
+* Addition operator :- Add two  value.
+Ex:- a = 5
+     b = 3
+     c = a+b
+     print(c)
+o/p  - 8
+'/'  Division       --> Divides left hand operand by right hand operand
+'//' Floor Division	--> remove the decimal part.
+'%' Modulus        -->	Divides left hand operand by right hand operand and returns remainder
+2. Assignment operators - These are used to assign values to the variables like +=,-=,=,*=
+3. Comparison operators - Comparison operators are used to compare two values like ==, >,<, <=,>=
+4. Logical operators    - Logical operators are used to combine conditional statements like And or not.
+5. Identity operators   - Identity operators are used to compare the memory address of two object
+6. Membership operators - Membership operators are used " To check weather the specified value present in
+                          iterable object or not".
+7. Bitwise operatorss-    Bitwise opreation works on bits and perform bit by bit operation.
+what is different between == vs is operator.
+ ==vs is:- == is for comparing the two values and 'is' for the memory location comparison.
+"""
