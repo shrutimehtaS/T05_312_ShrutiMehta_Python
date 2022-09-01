dc232f76fe64560a29c357067127cfd7561f2055
shrutimehtaS <111172882+shrutimehtaS@users.noreply.github.com>
2022-09-02 01:02:39 +0530

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,56 @@
+"""
+1. what is python.
+
+* Python is an interpreted,object-oriented and high-level programming language.
+* Interpreter- It means the source code of a Python program is converted into
+  bytecode that is then executed by the Python virtual machine.
+* High-level language:-High level language use english words to develop program.These are easy to learn and use.
+========================================================================================================================
+2. Application of  python.
+
+* It is used to build websites and software.
+* automate tasks and conduct data analysis.
+* machine learning ,datascience and automation
+========================================================================================================================
+3. Feature of python.
+
+ feature of python are -
+*Free and Open Source
+======================:- python language is freely available at the official website and you can download it from the
+Google the Download Python keyword. Download Python Since it is open-source, this means that source code is also
+available to the public. So you can download it,use it as well as share it.
+* Simple
+========:- python is a simple programming language. when we read a  python program , we feel like reading english.It
+means more clarity and less stress on understanding the syntax of the language.
+* Easy to learn
+=================:- python uses very few keywords.Its programs use very simple structure.
+* Dynamically Typed
+====================:-python is a dynamic language because we need not to  decleare the datatype.Python execution
+ environment decides the data types depends on the value we are using in python program.
+*Interpreter
+=============:- Python is an interpreted language, which means the source code of a Python program is converted into
+ bytecode that is then executed by the Python virtual machine.
+*High level language
+=====================:- High level language use english words to develop program.These are easy to learn and use.
+*Garbage collection in python
+==============================:- It is a inbuilt memory, which store the un-wanted data and improves the memory
+ quality.
+4. ADVANTAGE OF PYTHON.
+========================
+1.Easy to Read. 2. Easy to write the code. 3.It has a very supporitve community.
+4.It is easy to learn.
+5.DISADVANTAGE OF PYTHON.
+==========================
+1. Python is a time consuming language. 2.It has low execution speed.
+3.It is absence in mobile application.
+6. WHAT IS .py vs .pyc files - .py files contain the source code of a program.
+ .pyc file contains the bytecode of  program. We get bytecode after compilation of .py file (source code).
+7. What is PVM.
+=================
+ The role of pvm is to convert the byte code instruction into machine code.
+
+8.WHAT IS MEMORY MANAGEMENT IN PYTHON.
+=======================================
+Memory allocation and deallocation are done by during run time automatically.
+
+"""
