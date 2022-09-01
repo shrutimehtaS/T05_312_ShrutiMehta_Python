cb7c42343a6e41f9321277b0582e8ff1767d7dde
shrutimehtaS <111172882+shrutimehtaS@users.noreply.github.com>
2022-09-02 01:04:09 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,23 @@
+""" (1.) VARIABLE:- variable is a container that contain the value.
+Ex:x=2
+X- variable
+2- value
+========================================================================================================================
+(2.) Tokens in Python. Explain all types.
+
+- A token is the smallest individual unit in a python program.
+1. Identifiers: Identifiers can be a combination of letters in lowercase or uppercase or digits or an underscore_.
+2. Keywords: A keyword are special reserved words that have specific meanings and purposes.
+3. Operators: Operators used to perform operations on variable and value .
+4. Constants: These are the  value we can't change.
+   x=10
+  here 10 is constant,the value we can't the change.
+5. Literals: A literal is a number or string that appears directly in a program.
+========================================================================================================================
+(3.) LOCAL VS GLOBAL VARIABLES .
+
+Variables are classified into Global variables and Local variables based on their scope.
+The  global variables can be accessed globally in the entire program,
+and local variables can be accessed only within the function or block.
+========================================================================================================================
+"""
