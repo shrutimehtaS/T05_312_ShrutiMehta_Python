79a4a1a1de8304c886e76aca1a5a7eee62f2fff6
shrutimehtaS <111172882+shrutimehtaS@users.noreply.github.com>
2022-09-02 01:01:05 +0530

    Add files via upload

---
+++ datatype introduction.py
@@ -0,0 +1,16 @@
+"""Data type :- Datatype represent the type of data store into a variable or memory.
+python have mainly 3 type of data type-
+1.numeric. 2.Boolean. 3.string
+1.Numeric:- The numeric type represent numbers.They are 4 types of-
+a.) Integer:-int datatype represent an integer number.
+Ex:- 2,5,4.
+b.)Float:- The float datatype represent floating point number.
+Ex:- 2.5,3.5
+c.)Complex:- The complex number is a number that is written in the form of a+bj.here a is the real number and b is the
+             imaginary number.
+Ex:- a+bj
+d.)long:- we are not using in python.
+2.)Boolean:-This type of datatype we can use for binary categorical value.
+Ex:- TRUE or FALSE.
+3.)string:-string is a collection of alphabets.
+"""
