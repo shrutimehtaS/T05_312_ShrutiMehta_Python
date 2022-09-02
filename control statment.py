ae95e7661cc6abcab66db725b6dc326b0d679277
shrutimehtaS <111172882+shrutimehtaS@users.noreply.github.com>
2022-09-02 15:52:14 +0530

    Add files via upload

---
+++ control statment.py
@@ -0,0 +1,48 @@
+"""
+What is Control statements.
+
+* Control statement ara statement which control or change the flow of execution .
+the following are the control statement available in python.
+* Break statement
+* pass statement
+* continue statement
+==============================================================================
+What is break statement.
+
+* Break statement can be used inside a for loop or while loop to come out of the loop.
+ Ex
+ ===:- x = 10
+       while x>=1:
+           print('x=',x)
+           x- = 1
+           if x==5:
+              break
+       print("out of loop")
+===================================================================================
+What is pass statement.
+
+* pass statement dose not do anything.It is used with 'if' statement
+or inside a loop to represent no operation.
+
+Ex
+===:- x = 0
+      while x < 10:
+          x+ = 1
+          if  x>5:
+             pass
+         print(' x=',x)
+      print(" out of loop")
+======================================================================================
+ What is continue statement.
+
+*  These continue statement is used in a loop to go back to the begging of the loop.
+Ex
+===:- x= 0
+     while x<10:
+        x+ = 1
+        if x>5:
+            continue
+        print('x=',x)
+     print("out of loop")
+==========================================================================================
+"""
